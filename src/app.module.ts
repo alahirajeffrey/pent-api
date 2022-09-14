c9242436735aaa643f90d6acbd5a632a9e8c5512
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-14 15:51:08 +0100

    added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from 'ormconfig';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal:true}),
     TypeOrmModule.forRoot(AppDataSource.options),
-    AuthModule],
+    AuthModule,
+    UserModule],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+ 
