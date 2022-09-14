9185551cac31124fca28648042aa81fa34524a91
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-14 15:50:36 +0100

    added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true
+  }))
 
   const config = new DocumentBuilder()
     .setTitle('Pent API')
     .setDescription('The Pent API endpoints documentation')
     .setVersion('1.0')
-    .addTag('Pent API')
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
