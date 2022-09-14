f8515e21940c97f21442ef612dadb9eda8b73eba
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-14 15:51:29 +0100

    setup auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from './dto';
 
+@ApiTags("Auth")
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService : AuthService
+    ){}
+
+    @Post('register')
+    register(@Body() registerDto : RegisterDto){
+        return this.authService.register(registerDto)
+    }
+
+    @Post('login')
+    login(@Body() loginDto : LoginDto){
+        return this.authService.login(loginDto)
+    }
+}
