d835d60b94379b123a03a804d815d6a712e019f7
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-23 15:35:47 +0100

    added dto to update user type

---
+++ src/user/dto/update-user-type.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+import { UserType } from "src/common/enums/user-type.enum";
+
+
+export class UpdateUserTypeDto{
+    
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    type: UserType
+}   
