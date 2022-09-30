9a928a30d16d971132c4649ebce438d34806002b
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-30 18:55:23 +0100

    updated dto

--- src/reviews/dto/update-reviews.dto.ts
+++ src/reviews/dto/update-reviews.dto.ts
@@ -1,19 +1,23 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsString } from "class-validator"
+import { IsOptional, IsString } from "class-validator"
 
 export class UpdateReviewDto {
     @ApiProperty()
     @IsString()
+    @IsOptional()
     apartmentReview: string 
 
+    @IsOptional()
     @ApiProperty()
     @IsString()
     environmentReview: string
 
+    @IsOptional()
     @ApiProperty()
     @IsString()
     amenitiesReview: string
 
+    @IsOptional()
     @ApiProperty()
     @IsString()
     landlordReview: string
