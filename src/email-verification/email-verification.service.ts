ef037007b52237e7ca3ecd9129ce6d3695e8e41d
alahirajeffrey <alahirajeffrey@gmail.com>
2022-10-17 22:12:02 +0100

    setup mail service

--- src/email-verification/email-verification.service.ts
+++ src/email-verification/email-verification.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createTransport } from 'nodemailer';
+import * as Mail from 'nodemailer/lib/mailer';
 
 @Injectable()
-export class EmailVerificationService {}
+export class EmailVerificationService {
+    private nodeMailerTransport: Mail
+    constructor(
+        private readonly configService : ConfigService
+    ){
+        //setup mail service provider
+        this.nodeMailerTransport = createTransport({
+            service: configService.get('EMAIL_SERVICE'),
+            auth: {
+                user: configService.get('EMAIL_USER'),
+                pass: configService.get('EMAIL_PASSWORD')
+            }
+        })
+    }
+
+    sendMail(options: Mail.Options){
+        return this.nodeMailerTransport.sendMail(options)
+    }
+}
