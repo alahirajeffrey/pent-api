5ace5d6821b7b434a7bf7a2db36d6b3f28274d9a
alahirajeffrey <alahirajeffrey@gmail.com>
2022-10-17 22:12:25 +0100

    setup email verification module

--- src/email-verification/email-verification.module.ts
+++ src/email-verification/email-verification.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailVerificationService } from './email-verification.service';
-import { EmailVerificationController } from './email-verification.controller';
+
+
 
 @Module({
+  imports:[EmailVerificationModule],
   providers: [EmailVerificationService],
-  controllers: [EmailVerificationController]
+
 })
 export class EmailVerificationModule {}
