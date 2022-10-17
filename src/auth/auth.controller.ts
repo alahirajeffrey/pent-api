ad72e5d81cc4adcf2a5948403e688b7d9ad8e9c5
alahirajeffrey <alahirajeffrey@gmail.com>
2022-10-17 14:15:53 +0100

    added route to change password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @ApiTags("Auth")
 @Controller('auth')
@@ -18,5 +19,13 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto : LoginDto){
         return this.authService.login(loginDto)
+    }
+
+    @Post('passwordChange')
+    changePassword(
+        @Body() dto: ChangePasswordDto
+    ){
+        this.authService.changePassword(dto)
+    return {message: "Password changed"}
     }
 }
