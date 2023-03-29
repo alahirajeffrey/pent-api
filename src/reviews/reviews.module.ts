9890b4a86595275c685568fb10895af635e41519
alahirajeffrey <alahirajeffrey@gmail.com>
2023-03-29 17:00:32 +0100

    code refactor

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Apartment } from 'src/entities/apartment.entity';
-import { Review } from 'src/entities/review.entity';
-import { User } from 'src/entities/user.entity';
-import { UserService } from 'src/user/user.service';
+import { Apartment } from '../entities/apartment.entity';
+import { Review } from '../entities/review.entity';
+import { User } from '../entities/user.entity';
+import { UserService } from '../user/user.service';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Review, Apartment, User])],
   controllers: [ReviewsController],
-  providers: [ReviewsService, UserService]
+  providers: [ReviewsService, UserService],
 })
 export class ReviewsModule {}
