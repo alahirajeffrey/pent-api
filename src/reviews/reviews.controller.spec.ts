126fe1b7830462eded0d76b1ff847c2d8af68737
alahirajeffrey <alahirajeffrey@gmail.com>
2023-03-29 15:30:16 +0100

    unit tests for reviews controller

--- src/reviews/reviews.controller.spec.ts
+++ src/reviews/reviews.controller.spec.ts
@@ -1,18 +1,70 @@
+import { AuthGuard } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../user/user.service';
 import { ReviewsController } from './reviews.controller';
+import { ReviewsService } from './reviews.service';
 
 describe('ReviewsController', () => {
   let controller: ReviewsController;
 
+  const mockReviewsService = {
+    addReview: jest.fn((dto) => {}),
+    editReview: jest.fn(),
+    viewAllApartmentReviews: jest.fn((id) => {}),
+    deleteReview: jest.fn(),
+  };
+
+  const mockUserService = {
+    addReview: jest.fn((dto) => {}),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReviewsController],
-    }).compile();
+      providers: [ReviewsService, UserService],
+    })
+      .overrideProvider(ReviewsService)
+      .useValue(mockReviewsService)
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<ReviewsController>(ReviewsController);
   });
 
-  it('should be defined', () => {
+  it('controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create review', () => {
+    const dto = {
+      apartmentId: '12345',
+      apartmentReview: 'good',
+      reviewerId: '54321',
+      environmentReview: 'good',
+      amenitiesReview: 'good',
+      landlordReview: 'good',
+    };
+
+    it('should call the createReview service', async () => {
+      expect(await controller.addReview(dto)).toHaveBeenCalled();
+      expect(await controller.addReview(dto)).toHaveBeenCalledWith(dto);
+    });
+
+    it('should return return an object', async () => {
+      expect(await controller.addReview(dto)).toEqual({});
+    });
+  });
+
+  describe('view all apartment reviews', () => {
+    const id = '12345';
+    it('should call viewAllApartmentReviews service', () => {
+      expect(controller.viewAllApartmentReviews(id)).toHaveBeenCalled();
+      expect(controller.viewAllApartmentReviews(id)).toHaveBeenCalledWith(id);
+    });
+
+    it('should return an object', () => {
+      expect(controller.viewAllApartmentReviews(id)).toEqual({});
+    });
   });
 });
