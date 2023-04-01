f6396611907e06d8f202af87bcca053408ad2ccb
alahirajeffrey <alahirajeffrey@gmail.com>
2023-04-01 18:23:18 +0100

    code refactor

--- src/user/dto/update-user-type.dto.ts
+++ src/user/dto/update-user-type.dto.ts
@@ -1,12 +1,10 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
-import { UserType } from "src/common/enums/user-type.enum";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { UserType } from '../../common/enums/user-type.enum';
 
-
-export class UpdateUserTypeDto{
-    
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    type: UserType
-}   
+export class UpdateUserTypeDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  type: UserType;
+}
